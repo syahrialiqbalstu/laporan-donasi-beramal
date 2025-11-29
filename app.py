06ae2d4dadaa9a704eb6b06d2d8a09c9082ecab0
syahrialiqbalstu <syahrialiqbal@student.uns.ac.id>
2025-11-29 23:52:09 +0700

    Refactor app.py to improve structure and styling

--- app.py
+++ app.py
@@ -224,7 +224,7 @@ if uploaded_file is not None:
                             </div>
                         </div>
                         """,
-                        unsafe_allow_html=True
+                        unsafe_allow_html=True,
 
                          # Tombol kirim – akan disable jika sudah dicentang
                     st.link_button(
@@ -241,6 +241,7 @@ if uploaded_file is not None:
         st.error(f"Terjadi kesalahan: {e}")
 else:
     st.info("Silakan upload file di menu sebelah kiri (Sidebar).")
+
 
 
 
